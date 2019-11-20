0d8cacfdfae6b4bc179c7176cc1775fae0aab67c
yxj0312 <yanxiajun@hotmail.com>
2019-11-20 23:44:47 +0100

    S2 Ep 08 Routing to Controllers

--- routes/web.php
+++ routes/web.php
@@ -11,22 +11,11 @@
 |
 */
 
+use App\Http\Controllers\PostsController;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-Route::get('/post/{post}', function ($post) {
-    $posts = [
-        'my-first-post' => 'Hello, this is my first blog post!',
-        'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-    ];
-
-    if (!array_key_exists($post, $posts)) {
-        abort(404, 'Sorry, that post was not found.');
-    }
-
-    return view('post', [
-        'post' => $posts[$post]
-    ]);
-});
+Route::get('/post/{post}', 'PostsController@show');
