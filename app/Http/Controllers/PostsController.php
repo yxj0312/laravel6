e9b3dea7cd4ecc492bc4a8a2173e57eba7de0bad
yxj0312 <yanxiajun@hotmail.com>
2019-11-21 20:51:41 +0100

    S3 Ep 10 Hello Eloquent... Add a model and change controller, wait for next ep for migration

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,23 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    public function show($post)
+    public function show($slug)
     {
-        $posts = [
-            'my-first-post' => 'Hello, this is my first blog post!',
-            'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-        ];
-
-        if (!array_key_exists($post, $posts)) {
-            abort(404, 'Sorry, that post was not found.');
-        }
+        $post = Post::where('slug', $slug)->firstOrFail();
+        // $posts = [
+        //     'my-first-post' => 'Hello, this is my first blog post!',
+        //     'my-second-post' => 'Now I am getting the hang of this blogging thing.'
+        // ];
 
         return view('post', [
-            'post' => $posts[$post]
+            'post' => $post
         ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -9,6 +9,6 @@
     <body>
         <h1>My Blog Post</h1>
         
-        <p>{{ $post }}</p>
+        <p>{{ $post->body }}</p>
     </body>
 </html>
