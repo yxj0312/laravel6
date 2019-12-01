a311c4763310e3340eb06a9719e27b56d862b5e5
yxj0312 <yanxiajun@hotmail.com>
2019-12-01 21:20:43 +0100

    S5 Ep 21 The Seven Restful Controller Actions

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -20,4 +20,30 @@ class ArticlesController extends Controller
 
         return view('articles.show', ['article' => $article]);
     }
+
+    public function create()
+    {
+        
+    }
+
+    public function store()
+    {
+        
+    }
+
+    public function edit()
+    {
+        
+    }
+
+    public function update()
+    {
+        
+
+    }
+
+    public function destroy()
+    {
+        
+    }
 }
