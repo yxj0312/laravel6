65ee3c34292871863cad8adc26f46875225bdbb3
yxj0312 <yanxiajun@hotmail.com>
2019-12-17 20:21:35 +0100

    Add test example and config webpack

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <div>Example Component</div>
+        <div>I'm an example component!</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted')
+        },
+    }
+</script>
---
+++ tests/JavaScript/example.spec.js
@@ -0,0 +1,12 @@
+  
+import { mount } from '@vue/test-utils';
+import expect from 'expect';
+import Example from '../../resources/js/components/ExampleComponent.vue';
+
+describe ('Example', () => {
+    it ('says that it is an example component', () => {
+        let wrapper = mount(Example);
+
+        expect(wrapper.html()).toContain('Example Component');
+    });
+});
