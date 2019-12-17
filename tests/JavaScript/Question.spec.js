8e2bc36c4be95574db6b8591f6f633281340a760
yxj0312 <yanxiajun@hotmail.com>
2019-12-17 22:26:50 +0100

    Testingvue Ep 7... TDD a Question Component With Helper Functions --00:27

---
+++ tests/JavaScript/Question.spec.js
@@ -0,0 +1,11 @@
+import { mount } from '@vue/test-utils';
+import expect from 'expect';
+import Question from '../../resources/js/components/Question.vue';
+
+describe ('Question', () => {
+   let wrapper;
+
+    beforeEach(()=> {
+        wrapper = mount(Question);
+    });
+});
