e4357119086eea01dbf9e590d2cecb1cb4c26839
yxj0312 <yanxiajun@hotmail.com>
2019-12-31 20:37:19 +0100

    Update test

--- tests/JavaScript/Question.spec.js
+++ tests/JavaScript/Question.spec.js
@@ -3,21 +3,79 @@ import expect from 'expect';
 import Question from '../../resources/js/components/Question.vue';
 
 describe ('Question', () => {
-   let wrapper;
+    let wrapper;
 
-    beforeEach(()=> {
+    beforeEach (() => {
         wrapper = mount(Question, {
             propsData: {
-                question: {
-                    title:'The title',
+                dataQuestion: {
+                    title: 'The title',
                     body: 'The body'
                 }
             }
         });
     });
 
-    it('presents the title and the body', ()=> {
-        expect(wrapper.html()).toContain('The title');
-        expect(wrapper.html()).toContain('The body');
+    it ('presents the title and the body', () => {
+        see('The title');
+        see('The body');
     });
+
+    it ('can trigger edit mode', () => {
+        expect(wrapper.contains('input[name=title]')).toBe(false);
+
+        click('#edit');
+
+        expect(wrapper.find('input[name=title]').element.value).toBe('The title');
+        expect(wrapper.find('textarea[name=body]').element.value).toBe('The body');
+    });
+
+    it ('hides the edit button during edit mode', () => {
+        expect(wrapper.contains('#edit')).toBe(true);
+
+        click('#edit');
+
+        expect(wrapper.contains('#edit')).toBe(false);
+    });
+
+    it ('updates the question after being edited', () => {
+        click('#edit');
+
+        type('Changed title', 'input[name=title]');
+        type('Changed body', 'textarea[name=body]');
+        
+        click('#update');
+        
+        see('Changed title');
+        see('Changed body');
+    });
+
+    it ('can cancel out of edit mode', () => {
+        click('#edit');
+
+        type('Changed title', 'input[name=title]');
+
+        click('#cancel');
+
+        see('The title');
+    });
+
+    // Helper Functions
+
+    let see = (text, selector) => {
+        let wrap = selector ? wrapper.find(selector) : wrapper;
+
+        expect(wrap.html()).toContain(text);
+    };
+
+    let type = (text, selector) => {
+        let node = wrapper.find(selector);
+
+        node.element.value = text;
+        node.trigger('input');
+    };
+
+    let click = selector => {
+        wrapper.find(selector).trigger('click');
+    };
 });
